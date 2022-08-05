b4eed352584ae52593fa79f3e785e3e3dba611af
liberal-henrique <luihenlibsou@gmail.com>
2022-08-05 15:41:47 +0100

    first commit

---
+++ app/models/negociacoes.ts
@@ -0,0 +1,13 @@
+import Negociacao from "./Negociacao.js";
+
+export class Negociacoes {
+  private negociacoes: Negociacao[] = [];
+
+  public adiciona(negociacao: Negociacao) {
+    this.negociacoes.push(negociacao);
+  }
+
+  public lista(): readonly Negociacao[] {
+    return this.negociacoes;
+  }
+}
